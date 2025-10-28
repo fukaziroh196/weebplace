e81036fe0d92582d5b7dc0d0599eea420f2e13f1
fukaziroh196 <sonnienoie@gmail.com>
2025-10-28 15:49:08 +0700

    Update AdminQuizPanel.svelte

--- src/components/AdminQuizPanel.svelte
+++ src/components/AdminQuizPanel.svelte
@@ -101,7 +101,7 @@
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${localStorage.getItem('token')}`
+          'Authorization': `Bearer ${localStorage.getItem('api_token')}`
         },
         body: JSON.stringify({
           quizDate: adminUploadDate,
