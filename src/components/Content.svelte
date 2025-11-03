baa1aa4bbafbea2e6af35373114ea6580e845d6f
fukaziroh196 <sonnienoie@gmail.com>
2025-11-04 02:38:53 +0700

    Update Content.svelte

--- src/components/Content.svelte
+++ src/components/Content.svelte
@@ -287,7 +287,7 @@
       0 4px 12px rgba(0, 0, 0, 0.15),
       inset 0 1px 0 rgba(255, 255, 255, 0.1);
     width: 120px;
-    height: 120px;
+    height: 138px;
     display: flex;
     align-items: center;
     justify-content: center;
