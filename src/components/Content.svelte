632a6a5e65d6e3e10d9a9f330c6f31cd17cd924d
fukaziroh196 <sonnienoie@gmail.com>
2025-12-22 21:20:06 +0700

    Update Content.svelte

--- src/components/Content.svelte
+++ src/components/Content.svelte
@@ -1405,10 +1405,10 @@ $: playersToday = $userStats?.data?.playersToday ?? 3456;
   }
 
   .global-stats-panel {
-    width: clamp(220px, 24vw, 290px);
+    width: clamp(230px, 25vw, 300px);
     flex-shrink: 0;
     flex-grow: 0;
-    margin-left: clamp(6rem, 12vw, 20rem);
+    margin-left: clamp(6.5rem, 12vw, 20rem);
     background: var(--news-panel-gradient);
     border-radius: 1.625rem;
     padding: 1.25rem;
